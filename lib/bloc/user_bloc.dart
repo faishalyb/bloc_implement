17494c45db8f581cfada9e2718482fefe045f374
faishalyb <faishalyb@student.telkomuniversity.ac.id>
2024-09-04 16:55:49 +0700

    bloc of user done

---
+++ lib/bloc/user_bloc.dart
@@ -0,0 +1,23 @@
+import 'package:bloc_implement/data/datasource/remote_datasource.dart';
+import 'package:bloc_implement/data/model/user.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'user_event.dart';
+part 'user_state.dart';
+
+class UserBloc extends Bloc<UserEvent, UserState> {
+  final RemoteDatasource remoteDatasource;
+
+  UserBloc(this.remoteDatasource) : super(UserInitial()) {
+    on<UserEvent>((event, emit) async {
+      emit(UserLoading());
+      try {
+        final result = await remoteDatasource.getUsers();
+        emit(UserLoaded(result.data));
+      } catch (e) {
+        emit(UserError(e.toString()));
+      }
+    });
+  }
+}
